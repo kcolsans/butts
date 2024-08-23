b0e970b680fe4c09328758ef7d1b7f712ddfe3d3
kcolsans <kcolemilyjane@icloud.com>
2024-08-23 16:17:53 +0100

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1IC6ZakwjckFDWrmkcBqBiHBDSE8ggEgW8WL7XQZoFg4";
 
 
 /* ------------------------------------------------------------------- */
@@ -11,14 +11,6 @@ let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
 let sheetPages = {
 
     masterlist: "masterlist",
-    masterlistLog: "masterlist log",
-    inventory: "inventory",
-    inventoryLog: "inventory log",
-    items: "items",
-    traits: "traits",
-    prompts: "prompts",
-    faq: "faq",
-    staff: "mods",
 
 }
 
@@ -57,94 +49,6 @@ let options = {
 
         filterColumn: 'Design Type',
         searchFilterParams: ['ID', 'Owner', 'Designer', 'Artist'],
-        fauxFolderColumn: 'Species',
+        fauxFolderColumn: 'MYO Type',
 
-    },
-
-
-    /* Item Catalogue
     /* --------------------------------------------------------------- */
-    items: {
-
-        sheetPage: sheetPages.items,
-    
-        itemAmount: 24,
-        itemOrder: "asc",
-    
-        filterColumn: 'Rarity',
-        searchFilterParams: ['Item'],
-        fauxFolderColumn: 'Type',
-    
-    },
-
-
-    /* Invetory
-    /* --------------------------------------------------------------- */
-    inventory: {
-
-        sheetPage: sheetPages.inventory,
-        itemSheetPage: sheetPages.items,
-        logSheetPage: sheetPages.inventoryLog,
-    
-        itemAmount: 24,
-        sortTypes: ['Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-        
-        searchFilterParams: ['Username'],
-    
-    },
-
-
-    /* Prompts
-    /* --------------------------------------------------------------- */
-    prompts: {
-    
-        sheetPage: sheetPages.prompts,
-
-        itemAmount: 24,
-        itemOrder: "desc",
-
-        searchFilterParams: ['Title'],
-    
-    },
-
-
-    /* Traits
-    /* --------------------------------------------------------------- */
-    traits: {
-    
-        sheetPage: sheetPages.traits,
-
-        itemAmount: 24,
-        itemOrder: "asc",
-
-        filterColumn: 'Rarity',
-        searchFilterParams: ['Trait'],
-        fauxFolderColumn: 'Type',
-    
-    },
-
-
-    /* Staff
-    /* --------------------------------------------------------------- */
-    staff: {
-    
-        sheetPage: sheetPages.staff,
-    
-    },
-
-
-    /* FAQ
-    /* --------------------------------------------------------------- */
-    faq: {
-    
-        sheetPage: sheetPages.faq,
-    
-        itemAmount: 24,
-        itemOrder: "asc",
-    
-        searchFilterParams: ['Tags'],
-    
-    },
-
-
-}
